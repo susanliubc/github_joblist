44b48cfbb3489ff2f153f1dee3faf5b72c70748d
susanliubc <susanliubc@gmail.com>
2020-08-01 18:52:13 -0700

    fix checkbox

--- joblist/src/App.js
+++ joblist/src/App.js
@@ -1,29 +1,57 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
 import './App.css';
 import { Container } from 'react-bootstrap';
 import SearchForm from './components/SearchForm';
 import useFetchJobs from './utils/hooks/useFetchJobs';
+import { debounce } from 'throttle-debounce';
 import Job from './components/Job';
 
 function App() {
-  const [params, setParams] = useState({});
+  const [params, setParams] = useState({
+    description: '',
+    location: '',
+    full_time: false
+  });
   const [page, setPage] = useState(1);
   const { jobs, loading, error, hasNextPage } = useFetchJobs(params, page);
 
-  const handleParamChange = e => {
+  const handleParamChange = event => {
+    //debugger;
+    event.persist();
+
     setPage(1);
-    setParams(prevParams => ({
-      ...prevParams,
-      [e.target.name]: e.target.value
-    }));
+    //delayedQuery(event);
+    setParams(params =>
+      event.target.name === 'full_time'
+        ? { ...params, [event.target.name]: !params['full_time'] }
+        : {
+            ...params,
+            [event.target.name]: event.target.value
+          }
+    );
   };
 
+  // const delayedQuery = useCallback(
+  //   debounce(300, event =>
+  //     setParams(params =>
+  //       event.target.name === 'full_time'
+  //         ? { ...params, [event.target.name]: !params['full_time'] }
+  //         : {
+  //             ...params,
+  //             [event.target.name]: event.target.value
+  //           }
+  //     )
+  //   ),
+  //   []
+  // );
+
   console.log('jobs', jobs);
+  console.log('params', params);
 
   return (
     <Container className='my-4'>
       <h1 className='mt-4 text-center text-info'>Github Joblist</h1>
-      {/* <SearchForm params={params} onParamChange={handleParamChange} /> */}
+      <SearchForm params={params} onParamChange={handleParamChange} />
       {loading && <h2>Page loading</h2>}
       {error && (
         <h2 className='text-danger'>Error occurs. Please refresh the page</h2>
--- joblist/src/components/SearchForm.js
+++ joblist/src/components/SearchForm.js
@@ -1,5 +1,48 @@
 import React from 'react';
+import { Form, Col } from 'react-bootstrap';
 
-const SearchForm = () => {};
+const SearchForm = ({ params, onParamChange }) => {
+  return (
+    <Form className='mt-4'>
+      <Form.Row className='align-items-end'>
+        <Form.Group controlId='formDescription' as={Col}>
+          <Form.Label>Description</Form.Label>
+          <Form.Control
+            type='text'
+            name='description'
+            placeholder='Enter description'
+            onChange={onParamChange}
+            value={params.description}
+          />
+        </Form.Group>
+        <Form.Group controlId='formLocation' as={Col}>
+          <Form.Label>Location</Form.Label>
+          <Form.Control
+            type='text'
+            name='location'
+            placeholder='Enter Location'
+            onChange={onParamChange}
+            value={params.location}
+          />
+        </Form.Group>
+        <Form.Group
+          controlId='formBasicCheckbox'
+          as={Col}
+          xs='auto'
+          className='ml-2'
+        >
+          <Form.Check
+            type='checkbox'
+            onChange={onParamChange}
+            value={params.full_time}
+            name='full_time'
+            label='Only Full Time'
+            className='mb-2'
+          />
+        </Form.Group>
+      </Form.Row>
+    </Form>
+  );
+};
 
 export default SearchForm;
